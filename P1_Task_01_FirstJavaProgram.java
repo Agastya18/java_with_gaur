572c72262329d8eaa56a500fe9089d7ec6cc9c0b
Agastya Gaur <71458414+Agastya18@users.noreply.github.com>
2022-04-10 20:35:05 +0530

    Create P1_Task_01_FirstJavaProgram.java

---
+++ P1_Task_01_FirstJavaProgram.java
@@ -0,0 +1,8 @@
+package unit_01;
+
+public class P1_Task_01_FirstJavaProgram {
+	public static void main(String [] args) {
+		System.out.println("AGASTYA GAUR");
+		System.out.println("its my first program");
+	} 
+}
